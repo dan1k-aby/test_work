faa9022f3e62ab6429e736e9eabffbced439de4e
dan1k-aby <danik.aby@mail.ru>
2022-01-04 16:38:50 +0600

    bd

--- hierarchy/employee/views.py
+++ hierarchy/employee/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Employee, Chief
 from .serializers import EmployeeSerializer, ChiefSerializer
@@ -7,7 +9,11 @@ from .serializers import EmployeeSerializer, ChiefSerializer
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
-
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter)
+    filterset_fields = ('full_name', 'position', 'employment_date', 'salary',)
+    search_fields = ('full_name', 'position', 'employment_date', 'salary',)
+    ordering_fields = ('full_name', 'position', 'employment_date', 'salary',)
 
 class ChiefViewSet(viewsets.ModelViewSet):
     queryset = Chief.objects.all()
--- hierarchy/hierarchy/urls.py
+++ hierarchy/hierarchy/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from employee import views
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('employee.urls')),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
